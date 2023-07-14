59e68e9f95f1eabccd6ad06a211000b4a3005808
Uninstal <Danildd12345@mail.ru>
2023-07-15 04:31:42 +0500

    initial commit

---
+++ src/main/java/org/uninstal/seasons/Seasons.java
@@ -0,0 +1,23 @@
+package org.uninstal.seasons;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Seasons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
