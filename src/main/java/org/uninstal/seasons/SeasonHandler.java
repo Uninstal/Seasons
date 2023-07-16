ce97bbb774f185251d3479df773835bba45236cc
Uninstal <Danildd12345@mail.ru>
2023-07-16 21:32:14 +0500

    refactor user service

--- src/main/java/org/uninstal/seasons/SeasonHandler.java
+++ src/main/java/org/uninstal/seasons/SeasonHandler.java
@@ -5,7 +5,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.uninstal.seasons.data.SeasonUser;
 
 public class SeasonHandler implements Listener {
     
@@ -24,17 +23,11 @@ public class SeasonHandler implements Listener {
     
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        plugin.getDatabase().getUser(e.getPlayer().getName())
-          .thenAccept(plugin.getServices().getUserService()::add);
+        plugin.getServices().getUserService().load(e.getPlayer());
     }
     
     @EventHandler
     public void onLeave(PlayerQuitEvent e) {
-        SeasonUser user = plugin.getServices().getUserService().remove(e.getPlayer());
-        if (user == null) {
-            plugin.getLogger().severe("User is null on quit!?");
-        } else {
-            plugin.getDatabase().saveUser(user);
-        }
+        plugin.getServices().getUserService().unload(e.getPlayer());
     }
 }
--- src/main/java/org/uninstal/seasons/service/SeasonUserService.java
+++ src/main/java/org/uninstal/seasons/service/SeasonUserService.java
@@ -4,14 +4,13 @@ import org.bukkit.entity.Player;
 import org.uninstal.seasons.Seasons;
 import org.uninstal.seasons.data.SeasonUser;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 public class SeasonUserService {
 
     private final Seasons plugin;
+    private final Map<Player, CompletableFuture<Player>> loadings = new HashMap<>();
     private final Map<Player, SeasonUser> cached = new HashMap<>();
 
     public SeasonUserService(Seasons plugin) {
@@ -21,17 +20,28 @@ public class SeasonUserService {
     public Seasons getPlugin() {
         return plugin;
     }
-
-    public void add(SeasonUser user) {
-        this.cached.put(user.getPlayer(), user);
+    
+    public void load(Player player) {
+        loadings.put(player,
+          plugin.getDatabase().getUser(player.getName())
+            .thenApply(user -> {
+                loadings.remove(player);
+                if (player.isOnline()) {
+                    cached.put(player, user);
+                }
+                return player;
+            })
+        );
+    }
+    
+    public void unload(Player player) {
+        if (cached.containsKey(player)) {
+            plugin.getDatabase().saveUser(cached.remove(player));
+        }
     }
 
     public Optional<SeasonUser> get(Player player) {
         return Optional.ofNullable(this.cached.get(player));
-    }
-
-    public SeasonUser remove(Player player) {
-        return this.cached.remove(player);
     }
 
     public Collection<SeasonUser> getList() {
